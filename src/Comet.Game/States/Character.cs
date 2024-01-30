548d61dcd772e98afc8a5bec7009d2cbfc956fbb
CristianCiubancan <balooprogames@gmail.com>
2024-01-30 15:11:23 -0800

    lowered-dura-damage

--- src/Comet.Game/States/Character.cs
+++ src/Comet.Game/States/Character.cs
@@ -2503,7 +2503,7 @@ namespace Comet.Game.States
                 await SendGemEffect2Async();
 
             if (!Map.IsTrainingMap())
-                await DecEquipmentDurabilityAsync(true, (int) magic, (ushort) (power > MaxLife / 4 ? 10 : 1));
+                await DecEquipmentDurabilityAsync(true, (int) magic, (ushort) (power > MaxLife / 4 ? 3 : 1));
 
             if (MagicData.QueryMagic != null && MagicData.State == MagicData.MagicState.Intone)
                 await MagicData.AbortMagicAsync(true);
